666c98bb99f576e0865c9f8e0b28dfebbef9cbb6
Jorge Saud <jorgelsaud@gmail.com>
2018-04-18 06:40:35 -0300

    consulta fixed

--- resources/views/reservacion/admin/consulta.blade.php
+++ resources/views/reservacion/admin/consulta.blade.php
@@ -1,44 +1,62 @@
 @extends('templates.mainInterno')
 @section('content')
-    @include('templates.errors')
-    <h1>
-        Consultar Reserva:
-    </h1>
-    {!! Form::open(['id'=>'consultarReserva','method'=>'GET','url'=>route('consultarReservas')]) !!}
-    {!!
-ControlGroup::generate(
-Form::label('numero_de_reserva', 'Escriba el Numero de Reserva que Busca: '),
-Form::text('numero_de_reserva')
-)
-!!}
-    {!!
-ControlGroup::generate(
-Form::label('nombreoapellido', 'Escriba el Nombre o apellido que busca: '),
-Form::text('nombreoapellido')
-)
-!!}
-    {!!
-    ControlGroup::generate(
-    Form::label('fecha', 'Fecha del Paseo: '),
-    Form::text('fecha',null,['class'=>"datePicker"])
-    )
-    !!}
-
-    {!!
-ControlGroup::generate(
-Form::label('horas', 'Hora(s): '),
-Form::select('horas[]',$paseos,null,['class'=>'form-control','multiple'])
-)
-!!}
-    {!!
-ControlGroup::generate(
-Form::label('embarcaciones', 'Embarcacion(es): '),
-Form::select('embarcaciones[]',$embarcaciones,null,['class'=>'form-control','multiple'])
-)
-!!}
-
-
-    {!!Button::primary('Consultar')->large()->block()->submit()!!}
-    {!!Form::close()!!}
-
-@endsection
+@include('templates.errors')
+<h1>
+    Consultar Reserva:
+</h1>
+<form action="{{route('consultarReservas')}}" id="consultarReserva" method="GET">
+        {{csrf_field()}}
+    <div class="control-group">
+        <div class="form-group row">
+            <label for="numero_de_reserva" class="col-sm-2 col-form-label">Escriba el Numero de Reserva que Busca:</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" name="numero_de_reserva" id="numero_de_reserva" cols="30" rows="10"></input>
+            </div>
+        </div>
+    </div>
+    <div class="control-group">
+        <div class="form-group row">
+            <label for="nombreoapellido" class="col-sm-2 col-form-label">Escriba el Nombre o apellido que busca:</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" name="nombreoapellido" id="nombreoapellido" cols="30" rows="10"></input>
+            </div>
+        </div>
+    </div>
+    <div class="control-group">
+        <div class="form-group row">
+            <label for="fecha" class="col-sm-2 col-form-label">Fecha del Paseo:</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control datePicker" name="fecha" id="fecha2" cols="30" rows="10"></input>
+            </div>
+        </div>
+    </div>
+    <div class="control-group">
+        <div class="form-group row">
+            <label for="horas" class="col-sm-2 col-form-label">Hora(s):</label>
+            <div class="col-sm-10">
+                <select name="horas[]" class="form-control" id="horas" multiple>
+                    @foreach ($paseos as $key=>$paseo)
+                        <option value="{{$key}}" >{{$paseo}}</option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+    </div>
+    <div class="control-group">
+        <div class="form-group row">
+            <label for="embarcaciones" class="col-sm-2 col-form-label">Embarcacion(es):</label>
+            <div class="col-sm-10">
+                <select name="embarcaciones[]" class="form-control"  id="embarcaciones" multiple>
+                    @foreach ($embarcaciones as $key=>$embarcacion)
+                    <option value="{{$key}}" >{{$embarcacion}}</option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+    </div>
+<button type="submit" class="btn btn-primary">Consultar</button>
+</form>                        
+                        
+                        
+                        @endsection
+                        
--- resources/views/templates/assets/nav.blade.php
+++ resources/views/templates/assets/nav.blade.php
@@ -1,20 +1,16 @@
-{!!
-Navbar::top()
-->setType('navbar-puertorinoco')
-->fluid()
-->withBrand('<img class="logoNav" src="'.''.'"/>', '#')
-->withContent(
-	Navigation::links(
-		[
-			[
-				'link' => URL::route('inicio.index'),
-				'title' => 'Inicio'
-			],
-			[
-				'link' => URL::route('loginPanel'),
-				'title' => 'Panel Administrativo'
-			],
-		]
-	)
-)
-!!}
+<nav class="navbar navbar-expand fixed-top navbar-light bg-light">
+	<a class="navbar-brand" href="#"><img class="logoNav" src="{{Vari::get('logo')}}"/></a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<ul class="navbar-nav mr-auto">
+			<li class="nav-item active">
+				<a class="nav-link" href="{{URL::route('inicio.index')}}">Inicio</a>
+			</li>
+			<li class="nav-item active">
+				<a class="nav-link" href="{{URL::route('loginPanel')}}">Panel Administrativo</a>
+			</li>
+		</ul>
+	</div>
+</nav>
